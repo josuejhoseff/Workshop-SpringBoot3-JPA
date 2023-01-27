08ef553280f6407fb070c831cd2d7ade87a64b7d
Josué Jhoseff <josuejhosefff@gmail.com>
2023-01-27 11:05:21 -0300

    JPA respository, Injeção de dependência, Database seeding

---
+++ src/main/java/com/educand/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.educand.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educand.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
