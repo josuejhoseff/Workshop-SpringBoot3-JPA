f7ee5fade65f80de54940c4a8cf72840d1e6601e
Josué Jhoseff <josuejhosefff@gmail.com>
2023-01-27 14:50:33 -0300

    Entidade pedido, JsonIgnore, padrao ISO 8601

---
+++ src/main/java/com/educand/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.educand.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educand.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
---
+++ src/main/java/com/educand/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.educand.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.educand.course.entities.Order;
+import com.educand.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository userRepository;
+	
+	public List<Order> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = userRepository.findById(id);
+		return obj.get();
+	}
+}
