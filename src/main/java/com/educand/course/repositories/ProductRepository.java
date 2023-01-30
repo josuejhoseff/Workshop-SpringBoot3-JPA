2a97bd0dc6af858c16a30419da66d9d78cab3190
Josué Jhoseff <josuejhosefff@gmail.com>
2023-01-30 14:48:52 -0300

    Entidade Product

---
+++ src/main/java/com/educand/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.educand.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educand.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
